58617b907b6c8fd8523a662e943c19367ae7d322
meq1986 <meq1986@163.com>
2017-12-26 16:59:46 +0800

    增加usermodel类

--- src/com/maeq/memon/dao/UserDao.java
+++ src/com/maeq/memon/dao/UserDao.java
@@ -4,6 +4,7 @@
 package com.maeq.memon.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -37,6 +38,59 @@ public class UserDao {
 		return password;
 	}
 	
+	public int insertUser(String username,String password) throws SQLException, ClassNotFoundException
+	{
+		Connection con = null;
+        PreparedStatement ps = null;
+        try {
+            con = DBConn.getConnection();
+            ps = con.prepareStatement("insert into t_user (username,password) values (?,?)");
+            ps.setString(1, username);
+            ps.setString(2, password);
+            return ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+        	DBConn.closeConnection();
+        }
+        return -1;
+	}
+	
+	public int updateUserById(int id,String username,String password) throws SQLException
+	{
+		Connection con = null;
+        PreparedStatement ps = null;
+        try {
+            con = DBConn.getConnection();
+            ps = con.prepareStatement("update t_user set username = ? ,password=? where id=?");
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ps.setInt(3, id);
+            return ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+        	DBConn.closeConnection();
+        }
+        return -1;
+	}
+	
+	public int deleteUserById(int id) throws SQLException{
+		Connection con = null;
+        PreparedStatement ps = null;
+        try {
+            con = DBConn.getConnection();
+            ps = con.prepareStatement("delete from t_user where id=?");
+            ps.setInt(1, id);
+            return ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+        	DBConn.closeConnection();
+        }
+        return -1;
+	}
+	
 	/**
 	 * @param args
 	 * @throws SQLException 
@@ -44,6 +98,17 @@ public class UserDao {
 	public static void main(String[] args) throws SQLException {
 		UserDao userdao = new UserDao();
 		String password = userdao.getPassByName("maeq");
+		
+		try {
+			userdao.insertUser("hahaha", "12323123");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		userdao.updateUserById(4, "car", "wawa");
+		
+		userdao.deleteUserById(6);
 		
 		System.out.println(password);
 
