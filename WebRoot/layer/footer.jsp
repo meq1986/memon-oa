cb518578d8d4014da741d19013f774043d6d8e66
meq1986 <meq1986@163.com>
2018-02-21 08:27:33 +0800

    加入include特性

---
+++ WebRoot/layer/footer.jsp
@@ -0,0 +1,10 @@
+<%@ page language="java" import="java.util.*" pageEncoding="utf-8"%>
+
+        <!-- footer content -->
+        <footer>
+          <div class="pull-right">
+            MLentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
---
+++ src/com/maeq/memon/servlet/AddUserServlet.java
@@ -0,0 +1,90 @@
+package com.maeq.memon.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.maeq.memon.service.UserService;
+
+public class AddUserServlet extends HttpServlet {
+
+	private UserService us = new UserService();
+	
+	/**
+	 * Constructor of the object.
+	 */
+	public AddUserServlet() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		this.doPost(request, response);
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String email = request.getParameter("email");
+		String mobilephone = request.getParameter("mobilephone");
+		//int status = Integer.valueOf(request.getParameter("status"));
+		int status = 1;
+		String createdate = "2018-02-19 07:20";
+		
+		try {
+			us.insertUser(username, password, email, mobilephone, createdate, status);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
